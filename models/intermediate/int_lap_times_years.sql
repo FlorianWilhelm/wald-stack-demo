4e3fba4f720ea0dab0428a8c74ff4381a1042316
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2023-03-09 12:10:23 +0100

    Have driver_ref in int_lap_times_years

--- models/intermediate/int_lap_times_years.sql
+++ models/intermediate/int_lap_times_years.sql
@@ -10,16 +10,27 @@ races as (
 
 ),
 
+drivers as (
+
+  select * from {{ ref('stg_f1_drivers') }}
+
+),
+
 expanded_lap_times_by_year as (
     select
         lap_times.race_id,
-        driver_id,
+        lap_times.driver_id,
+        driver_ref,
         race_year,
+        race_name,
         lap,
-        lap_time_milliseconds
+        lap_time_milliseconds,
+        lap_time_milliseconds / 1000 as lap_time_seconds
     from lap_times
     left join races
         on lap_times.race_id = races.race_id
+    left join drivers
+        on lap_times.driver_id = drivers.driver_id
     where lap_time_milliseconds is not null
 )
 
